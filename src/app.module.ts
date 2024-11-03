b9bf90d7fca0b9327b3d64811653022f4e31c4a2
hirohana <hirohana1984@gmail.com>
2024-11-03 19:09:49 +0900

    controllerを作成
    	modified:   src/app.module.ts
    	modified:   src/cats/cats.controller.ts
    	new file:   src/cats/dto/createCatDto.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatModule } from './cat/cat.module';
 import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [CatModule],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  controllers: [CatsController],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,39 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/createCatDto';
 
 @Controller('cats')
 export class CatsController {
-  @Get()
-  findAll(@Req() request: Request): string {
-    return 'This Action returns all cats';
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
   }
 
-  @Post()
-  create(): string {
-    return 'This Action adds a new cat';
+  @Get()
+  findAll(@Query() query: any) {
+    return `This action returns all cats (limit: ${query.limit} items)`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
