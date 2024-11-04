156cd9938988685875185a7852dfa2ec3d34b927
hirohana <hirohana1984@gmail.com>
2024-11-04 10:03:17 +0900

    provider作成
    	modified:   src/app.module.ts
    	modified:   src/cats/cats.controller.ts
    	new file:   src/cats/cats.service.spec.ts
    	new file:   src/cats/cats.service.ts
    	new file:   src/cats/interfaces/cat.interface.ts

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,32 +8,22 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+
 import { CreateCatDto } from './dto/createCatDto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(@Query() query: any) {
-    return `This action returns all cats (limit: ${query.limit} items)`;
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string) {
-    return `This action updates a #${id} cat`;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
