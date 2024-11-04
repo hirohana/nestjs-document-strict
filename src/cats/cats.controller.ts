02a4fe227783a24f485b8ee78221cb0d85256fa2
hirohana <hirohana1984@gmail.com>
2024-11-04 14:08:53 +0900

    ExceptionFilterを作成
    	modified:   src/cats/cats.controller.ts
    	new file:   src/common/exceptionFilters/HttpExceptionFilter.ts

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,20 +1,50 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  UseFilters,
+  ForbiddenException,
+} from '@nestjs/common';
 
 import { CreateCatDto } from './dto/createCatDto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { HttpExceptionFilter } from 'src/common/exceptionFilters/HttpExceptionFilter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UseFilters(HttpExceptionFilter)
   async create(@Body() createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto);
+    // this.catsService.create(createCatDto);
+    throw new ForbiddenException();
   }
 
+  // @Get()
+  // async findAll(): Promise<Cat[]> {
+  //   return this.catsService.findAll();
+  // }
+
   @Get()
-  async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
+  async findAll() {
+    try {
+      return await this.catsService.findAll();
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'This is a custom message',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 }
