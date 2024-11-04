25df5efe1bb13ae0e907b4e5eaf01bf7bc943758
hirohana <hirohana1984@gmail.com>
2024-11-04 12:29:06 +0900

    Middleware作成
    	modified:   src/app.module.ts
    	new file:   src/common/middleware/LoggerMiddleware.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+
+import { logger } from './common/middleware/LoggerMiddleware';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
